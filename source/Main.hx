e3d2afbcbe8e76faf07e9d5e4c7d84d190bae56a
Maciej Kozłowski <mjp.kozlowski@gmail.com>
2020-07-01 09:39:26 +0200

    Remove flixel splash screen

--- source/Main.hx
+++ source/Main.hx
@@ -1,15 +1,12 @@
 package;
-import states.PlayState;
-import states.MenuState;
 
+import states.MenuState;
 import flixel.FlxGame;
 import openfl.display.Sprite;
 
-class Main extends Sprite
-{
-	public function new()
-	{
+class Main extends Sprite {
+	public function new() {
 		super();
-		addChild(new FlxGame(320, 180, MenuState));
+		addChild(new FlxGame(320, 180, MenuState, 1, 60, 60, true));
 	}
 }
